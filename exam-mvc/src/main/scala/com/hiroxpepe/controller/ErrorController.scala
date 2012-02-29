5d8b2d2d701bfbd298826d5c4fad4436c7f569d4
hiroxpepe <hiroxpepe@gmail.com>
2012-02-29 17:23:59 +0900

    moved i18n property files to WEB-INF.

--- exam-mvc/src/main/scala/com/hiroxpepe/controller/ErrorController.scala
+++ exam-mvc/src/main/scala/com/hiroxpepe/controller/ErrorController.scala
@@ -1,3 +1,17 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+
 package com.hiroxpepe.controller
 
 import org.slf4j.Logger
@@ -23,7 +37,7 @@ class ErrorController {
     
     ///////////////////////////////////////////////////////////////////////////
     /**
-     * if a error is occured, this method will be called.
+     * if an error is occured, this method will be called.
      */
     @ExceptionHandler
     def handleException(
@@ -35,7 +49,7 @@ class ErrorController {
     
     ///////////////////////////////////////////////////////////////////////////
     /**
-     * move to the view for a error.
+     * move to the view for an error.
      */
     @RequestMapping(
         value=Array("/error"),

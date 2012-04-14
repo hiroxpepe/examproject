84f1b7ae940a6872dc4c4cbd2d341f45bda37fc9
hiroxpepe <hiroxpepe@gmail.com>
2012-04-15 07:05:04 +0900

    add the import.sql file.

--- exmp-webapp/src/main/resources/import.sql
+++ exmp-webapp/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into PERSON (ID, CREATED, FIRST_NAME, LAST_NAME) values (1, null, 'John' 'Lennon')
-insert into PERSON (ID, CREATED, FIRST_NAME, LAST_NAME) values (2, null, 'Paul' 'McCartney')
-insert into PERSON (ID, CREATED, FIRST_NAME, LAST_NAME) values (3, null, 'George' 'Harrison')
-insert into PERSON (ID, CREATED, FIRST_NAME, LAST_NAME) values (4, null, 'Ringo' 'Starr')
+insert into PERSON (ID, CREATED, FIRST_NAME, LAST_NAME) values (1, null, 'John', 'Lennon')
+insert into PERSON (ID, CREATED, FIRST_NAME, LAST_NAME) values (2, null, 'Paul', 'McCartney')
+insert into PERSON (ID, CREATED, FIRST_NAME, LAST_NAME) values (3, null, 'George', 'Harrison')
+insert into PERSON (ID, CREATED, FIRST_NAME, LAST_NAME) values (4, null, 'Ringo', 'Starr')
